5493d58aec3d67ae9db1e09c999d0daa1d4aed5c
Cyril DRIMARACCI <cyrildrimaracci@live.fr>
2026-06-14 00:28:13 +0200

    add EOF, FILE, and fpos_t definitions in stdio.h

--- include/stdio.h
+++ include/stdio.h
@@ -1,5 +1,19 @@
 #pragma once
 
+#ifndef EOF
+#define EOF (-1)
+#endif
+
+#ifndef _FILE_DEFINED
+#define _FILE_DEFINED
+typedef struct _iobuf FILE;
+#endif
+
+#ifndef _FPOS_T_DEFINED
+#define _FPOS_T_DEFINED
+typedef __int64 fpos_t;
+#endif
+
 #define PRINTF_ALIAS_STANDARD_FUNCTION_NAMES_HARD 1
 #include <printf/printf.h>
 #undef PRINTF_ALIAS_STANDARD_FUNCTION_NAMES_HARD
